c107a2d1664ac0ef9a67aad6f074ab0d6cc49845
danieldiv <das.29@outlook.com>
2023-05-04 22:25:43 -0300

    get funcoes

--- semantico/semantico.py
+++ semantico/semantico.py
@@ -5,12 +5,12 @@ arq = open('files/code.txt','r')
 lista = []
 for x in arq:
     x = x.strip()
-    if(len(x) > 0 and not s.is_valid(x)): lista.append(x)
+    if(len(x) > 0 and s.is_valid(x)): lista.append(x)
 
 lista_include = []
 
-for x in lista:
-    print(x)
+# for x in lista:
+#     print(x)
 
 def getInclude(linha):
     if '#include' in linha:
@@ -22,26 +22,60 @@ def getInclude(linha):
     return False
 
 lista_funcoes = []
-reg_f = r'(int|float|void){1}\s[\w]+\s?\('
+lista_escopo = []
+reg_f1 = f'{s.reg_tipos.pattern}\w+\s*'
+
+escopo_atual = False
+sub_string = ''
+parametros = ''
 
 def getFuncoes(linha):
-    match = re.search(reg_f,linha)
-    if(match):
+    match = re.search(reg_f1,linha)
+    
+    global sub_string
+    global escopo_atual
+    global parametros
+    global lista_escopo
+    
+    if(match and '=' not in linha):
         sub_string = match.group()
-        linha = linha.replace(sub_string,'').replace(')','').replace('{','').strip()
-        sub_string = sub_string.replace('(','').strip()
+        linha = linha.replace(sub_string,'')
         
-        lista_funcoes.append([sub_string, linha])
+        if('{' in linha):
+            escopo_atual = True
+            linha = linha.replace('{','')
+        
+        if('(' in linha): linha = linha.replace('(','')
+        if(')' in linha): linha = linha.replace(')','')
+        
+        sub_string = sub_string.strip()
+        parametros = linha.strip()
+        
+        return True
+    elif('{' in linha):
+        escopo_atual = True
+        return True
+    elif('}' in linha):
+        lista_funcoes.append([[sub_string, parametros], lista_escopo])
+        escopo_atual = False
+        lista_escopo = []
+        return True
+    elif(escopo_atual):
+        lista_escopo.append(linha)
         return True
     return False
 
 for x in lista:
     if(getInclude(x)): continue
     elif(getFuncoes(x)): continue
-    # else: print('Erro na linha: '+x)
+    else: print('Erro na linha: '+x)
 
 for x in lista_funcoes:
-    print(x)
+    print(x[0])
+    for y in x[1]:
+        print(y)
+    print()
+    
    
 # lista_int = []
 # valor_int = []
